1233f636da09b8bc37707d9113fa623d078503f9
NeuralNetBot <console.logisaiah@gmail.com>
2023-01-20 17:18:51 -0800

    minor formatting changes to inputlistener

--- 320PhotoEditor/InputListener.h
+++ 320PhotoEditor/InputListener.h
@@ -4,12 +4,12 @@
 class InputListener
 {
 public:
-	virtual void keyPressed(sf::Keyboard::Key key);
-	virtual void keyReleased(sf::Keyboard::Key key);
-	virtual void mousePressed(sf::Mouse::Button button);
-	virtual void mouseReleased(sf::Mouse::Button button);
-	virtual void mouseScrolled(int delta);
-	virtual void mouseMoved(sf::Vector2i pos);
+	virtual void keyPressed(sf::Keyboard::Key key) {};
+	virtual void keyReleased(sf::Keyboard::Key key) {};
+	virtual void mousePressed(sf::Mouse::Button button) {};
+	virtual void mouseReleased(sf::Mouse::Button button) {};
+	virtual void mouseScrolled(int delta) {};
+	virtual void mouseMoved(sf::Vector2i pos) {};
 
 	//internal, do not use
 	void setWindow(sf::Window* window);
