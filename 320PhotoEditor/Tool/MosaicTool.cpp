24a851254600288d9faef5af05a4b66f56f99ae1
Joshua Mathwich <joshua.mathwich@wsu.edu>
2023-02-12 18:18:52 -0800

    Created selection window button.

--- 320PhotoEditor/Tool/MosaicTool.cpp
+++ 320PhotoEditor/Tool/MosaicTool.cpp
@@ -1,31 +1,56 @@
 #include "MosaicTool.h"
+#include "../GUI/GUIElement.h"
 
 MosaicTool::MosaicTool(sf::Texture* up, sf::Texture* down, sf::Texture* over) : Tool(up, down, over)
 {
 }
 
-void MosaicTool::start(Layer* layer)
+void MosaicTool::init()
 {
-	this->layer = layer;
-
     sf::Texture* upTexture = new sf::Texture();
-    upTexture->loadFromFile("../assets/mos_button_up.png");
+    upTexture->loadFromFile("../assets/button_up.png");
     sf::Texture* downTexture = new sf::Texture();
-    downTexture->loadFromFile("../assets/mos_button_down.png");
+    downTexture->loadFromFile("../assets/button_down.png");
     sf::Texture* overTexture = new sf::Texture();
-    overTexture->loadFromFile("../assets/mos_button_over.png");
+    overTexture->loadFromFile("../assets/button_over.png");
 
-    ButtonElement* button = new ButtonElement(upTexture, downTexture, overTexture);
-    button->setUpdateFunction([this](GUIElement* element, int status) { this->buttonPressed(element, status); });
-    container->addElement(button);
-
-    button->setSize({ .25, .25 });
-
-    button->setPosition({0, 0});
+    selectButton = new ButtonElement(upTexture, downTexture, overTexture);
+    selectButton->setUpdateFunction([this](GUIElement* element, int status) { this->buttonPressed(element, status); });
+    container->addElement(selectButton);
+    selectButton->setSize({ .25, .25 });
+    selectButton->setPosition({ 0.5, 0 });
 }
 
-void MosaicTool::keyPressed(sf::Keyboard::Key key)
+void MosaicTool::start(Layer* layer)
 {
-	layer->getImage()->setPixel(0, 0, sf::Color::Red); 
-	layer->reload();
+    this->layer = layer;
+}
+
+void MosaicTool::buttonPressed(GUIElement* button, int status)
+{
+    std::cout << button << "\n";
+    std::cout << selectButton << "\n";
+
+    if (status != ButtonElement::ButtonState::DOWN)
+    {
+        return;
+    }
+    
+if (button == selectButton) {
+
+        newWindow = new sf::RenderWindow(sf::VideoMode(500,500), "Test");
+        
+        while (newWindow->isOpen())
+        {
+            sf::Event event;
+            while (newWindow->pollEvent(event))
+            {
+                switch (event.type) {
+                case sf::Event::Closed:
+                    newWindow->close();
+                    break;
+                }
+            }
+        }
+    }
 }
--- 320PhotoEditor/Tool/MosaicTool.h
+++ 320PhotoEditor/Tool/MosaicTool.h
@@ -8,15 +8,17 @@ class MosaicTool : public Tool
 public:
 	MosaicTool(sf::Texture* up, sf::Texture* down, sf::Texture* over);
 
+	void init();
 	void start(Layer* layer);
 	void stop() {};
 	void run() {};
 
-	void keyPressed(sf::Keyboard::Key key);
+	//void keyPressed(sf::Keyboard::Key key);
 
-	void buttonPressed(GUIElement* button, int status) { }
+	void buttonPressed(GUIElement* button, int status);
 private:
-
+	ButtonElement* selectButton;
 	Layer* layer;
+	sf::RenderWindow* newWindow;
 };
 
