3ddabb55e3029fd52d648e396b77baf1d8baff25
NeuralNetBot <console.logisaiah@gmail.com>
2023-03-02 13:29:23 -0800

    added opengl and glew to project
    also added some stuff to compute shader
    
    opengl and glew should automatically work on windows and visual studio but mac will need some other linking

--- 320PhotoEditor/ComputeShader.cpp
+++ 320PhotoEditor/ComputeShader.cpp
@@ -5,8 +5,19 @@ ComputeShader::ComputeShader(const char* path)
 	compile(path);
 }
 
+ComputeShader::ComputeShader(unsigned int ID)
+{
+	this->ID = ID;
+}
+
 void ComputeShader::use()
 {
+	use(ID);
+}
+
+void ComputeShader::use(unsigned int ID)
+{
+	glUseProgram(ID);
 }
 
 void ComputeShader::compile(const char* path)
@@ -27,9 +38,29 @@ void ComputeShader::compile(const char* path)
 	std::string sshaderCode = shaderStream.str();
 	const char* shaderCode = sshaderCode.c_str();
 
+	unsigned int compute;
+	int success;
+	char infoLog[512];
+	compute = glCreateShader(GL_COMPUTE_SHADER);
+	glShaderSource(compute, 1, &shaderCode, NULL);
+	glCompileShader(compute);
+	checkCompileErrors(compute);
 
+	ID = glCreateProgram();
+	glAttachShader(ID, compute);
+	glLinkProgram(ID);
+	checkCompileErrors(compute);
+	glDeleteShader(compute);
 }
 
-void ComputeShader::checkCompileErrors()
+void ComputeShader::checkCompileErrors(unsigned int shader)
 {
+	GLint success;
+	char infoLog[1024];
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+		std::cerr << "Compute Shader Compile Error: " << "\n" << infoLog << std::endl;
+	}
 }
--- 320PhotoEditor/ComputeShader.h
+++ 320PhotoEditor/ComputeShader.h
@@ -4,18 +4,32 @@
 #include <fstream>
 #include <sstream>
 
+#ifdef _WIN32
+#include <Windows.h>
+#else
+
+#endif
+
+#include <GL/glew.h>
+#include <GL/GL.h>
+
 class ComputeShader
 {
 public:
 
 	ComputeShader(const char* path);
+	ComputeShader(unsigned int ID);
 
 	void use();
 
+	static void use(unsigned int ID);
+
 private:
+
+	unsigned int ID;
 
 	void compile(const char* path);
 
-	void checkCompileErrors();
+	void checkCompileErrors(unsigned int shader);
 };
 
