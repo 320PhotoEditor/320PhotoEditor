6779d143abc602537a0fdc602f406293f3edf165
NeuralNetBot <console.logisaiah@gmail.com>
2023-03-23 13:13:39 -0700

    some more test cases

--- Tests/Tests.cpp
+++ Tests/Tests.cpp
@@ -4,14 +4,33 @@
 #include <boost/test/included/unit_test.hpp>
 #include "../320PhotoEditor/Common.h"
 #include "../320PhotoEditor/AssetManager.h"
+#include "../320PhotoEditor/ComputeShader.h"
 
-BOOST_AUTO_TEST_CASE(TestAssetManager)
+BOOST_AUTO_TEST_CASE(AssetManager_Load_From_Mem)
 {
     AssetManager am = AssetManager::getInstance();
 
     sf::Texture* initalload = am.getTexture("../assets/button_up.png");
     sf::Texture* noload = am.getTexture("../assets/button_up.png");
 
-
     BOOST_CHECK_EQUAL(initalload, noload);
+}
+
+BOOST_AUTO_TEST_CASE(AssetManager_Load_Different)
+{
+    AssetManager am = AssetManager::getInstance();
+
+    sf::Texture* initalload = am.getTexture("../assets/button_up.png");
+    sf::Texture* difload = am.getTexture("../assets/button_down.png");
+
+    BOOST_CHECK_NE(initalload, difload);
+}
+
+BOOST_AUTO_TEST_CASE(ComputeShader_Load)
+{
+    //create the render window to init opengl context so compute shaders will run
+    sf::RenderWindow* rw = new sf::RenderWindow(sf::VideoMode(1280, 720), "window");
+
+    ComputeShader cs = ComputeShader("../assets/testcompute.comp");
+    BOOST_CHECK_NE(cs.ID, 0);
 }
