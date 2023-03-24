65a06ad74f6fad1ae95e9d8462bc9254ec753f52
davidoakes <davidoakes19888@gmail.com>
2023-03-24 13:19:53 -0700

    Test For Isaiah

--- Tests/Tests.cpp
+++ Tests/Tests.cpp
@@ -1,17 +1,53 @@
 #define BOOST_TEST_MODULE Test
 #define NOMINMAX
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MAIN
 
 #include <boost/test/included/unit_test.hpp>
 #include "../320PhotoEditor/Common.h"
 #include "../320PhotoEditor/AssetManager.h"
+#include "../320PhotoEditor/GUI/GUIContainer.h"
+#include "../320PhotoEditor/GUI/ButtonElement.h"
+#include "../320PhotoEditor/ApplicationMenu.h"
+#include "../320PhotoEditor/Tool/Zoom.h"
+#include "../320PhotoEditor/Application.h"
+#include "../320PhotoEditor/Tool/Tool.h"
+#include "../320PhotoEditor/Tool/ToolManager.h"
+#include <filesystem>
+#include <iostream>
+#include <string>
 
-BOOST_AUTO_TEST_CASE(TestAssetManager)
+
+BOOST_AUTO_TEST_CASE(Zoom_Window_Check)
 {
-    AssetManager am = AssetManager::getInstance();
+    sf::Texture* upTexture = new sf::Texture();
 
-    sf::Texture* initalload = am.getTexture("../assets/button_up.png");
-    sf::Texture* noload = am.getTexture("../assets/button_up.png");
+    sf::Image im = sf::Image();
+    im.create(100, 100, sf::Color::White);
+    upTexture->loadFromImage(im);
 
 
-    BOOST_CHECK_EQUAL(initalload, noload);
+    sf::RenderWindow *renderWindow = new sf::RenderWindow(sf::VideoMode(1000, 750), "windowName");
+    Layer *layer = new Layer({750, 500}, sf::Color::Black, renderWindow);
+    Zoom *zoomTool = new Zoom(upTexture, upTexture, upTexture);
+
+    GUIContainer *menuContainer = new GUIContainer({ 0, 0 }, { 1, 1 }, renderWindow);
+    zoomTool->setContainer(menuContainer);
+
+    zoomTool->init();
+    zoomTool->start(layer);
+
+    zoomTool->mouseMoved({500, 500});
+    zoomTool->getRenderWindow();
+
+    BOOST_CHECK(zoomTool->getRenderWindow()->isOpen());
+
 }
+
+
+
+
+
+
+
+
